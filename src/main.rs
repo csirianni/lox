d6e496f8d13a78db867f920cc218705e446da370
Cedric Sirianni <cedric.sirianni@mongodb.com>
2026-08-02 14:46:21 -0400

    fix: call run_file() with args[1]

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn main() -> io::Result<()> {
         println!("Usage: jlox [script]");
         std::process::exit(64);
     } else if args.len() == 2 {
-        lox.run_file(&args[0])?;
+        lox.run_file(&args[1])?;
     } else {
         lox.run_prompt()?;
     }
